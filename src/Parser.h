52880683c3d719fe054435277c111482c75f8a32
inf17101 <42932060+inf17101@users.noreply.github.com>
2022-02-14 15:59:22 +0100

    extracts index variable of parser to member

--- src/Parser.h
+++ src/Parser.h
@@ -30,116 +30,116 @@ namespace json
                 || literal == JsonLiteral::CLOSED_LIST_BRACE;
         }
 
-        std::tuple<std::vector<Json>, std::size_t, std::string> 
-            parseJsonArray(std::size_t index)
+        std::tuple<std::vector<Json>, std::size_t, std::string> parseJsonArray()
+        {
+            std::vector<Json> children;
+            auto amountOftokens_ { tokens_.size() };
+            while(index_ < amountOftokens_)
             {
-                std::vector<Json> children;
-                auto amountOftokens_ { tokens_.size() };
-                while(index < amountOftokens_)
+                auto token = tokens_[index_];
+                if(isSyntaxLiteral(token.type_))
                 {
-                    auto token = tokens_[index];
-                    if(isSyntaxLiteral(token.type_))
+                    if(token.type_ == JsonLiteral::CLOSED_LIST_BRACE)
+                        return {children, ++index_, {}};
+                    if(token.type_ == JsonLiteral::COMMA)
                     {
-                        if(token.type_ == JsonLiteral::CLOSED_LIST_BRACE)
-                            return {children, index + 1, {}};
-                        if(token.type_ == JsonLiteral::COMMA)
-                        {
-                            token = tokens_[index];
-                            index++;
-                        }else if(notEmpty(children))
-                        {
-                            return { {}, index, "Expected a comma between Json Array elements." };
-                        }
-                    }
-
-                    auto [child, newIndex, error] = parse(index);
-                    if(notEmpty(error))
-                        return { {}, index, error };
-                    
-                    children.push_back(std::move(child));
-                    index = newIndex;
-                }
-                return { {}, index, "Unexpected EOF while parsing array" };
-            }
-
-        std::tuple<std::map<std::string, Json>, std::size_t, std::string>
-            parseJsonObject(std::size_t index)
-            {
-                std::map<std::string, Json> values = {};
-                auto amountOftokens_ { tokens_.size() };
-                while(index < amountOftokens_)
-                {
-                    auto token = tokens_[index];
-                    if(isSyntaxLiteral(token.type_))
+                        token = tokens_[index_];
+                        index_++;
+                    }else if(notEmpty(children))
                     {
-                        if(token.type_ == JsonLiteral::CLOSED_CURLY_BRACE)
-                            return { values, index + 1, {} };
-
-                        if(token.type_ == JsonLiteral::COMMA)
-                        {
-                            index++;
-                            token = tokens_[index];
-                        }else if(notEmpty(values))
-                        {
-                            return { {}, index, "Expected comma after element in object." };
-                        }else
-                        {
-                            return { {}, index, "Expected key-value pair or closing brace in object." };
-                        }
+                        return { {}, index_, "Expected a comma between Json Array elements." };
                     }
-
-                    auto [key, newIndex, error] = parse(index);
-                    if(notEmpty(error))
-                        return { {}, index, error };
-                    
-                    if(key.type_ != JsonLiteral::STRING)
-                        return { {}, index, "Expected string key in object." };
-                    
-                    index = newIndex;
-                    token = tokens_[index];
-
-                    if(token.type_ != JsonLiteral::COLON)
-                        return { {}, index, "Expected colon after key in object." };
-                    
-                    index++;
-                    token = tokens_[index];
-                    auto [value, newIndex1, error1] = parse(index);
-                    if(notEmpty(error1))
-                        return { {}, index, error1 };
-                    values[key.string_.value()] = std::move(value);
-                    index = newIndex1;
                 }
-                return { {}, index + 1, "" };
-            }
 
-        std::tuple<Json, std::size_t, std::string> 
-            parse(std::size_t index = 0)
+                auto [child, newIndex, error] = parse();
+                if(notEmpty(error))
+                    return { {}, index_, error };
+                
+                children.push_back(std::move(child));
+                index_ = newIndex;
+            }
+            return { {}, index_, "Unexpected EOF while parsing array" };
+        }
+
+        std::tuple<std::map<std::string, Json>, std::size_t, std::string> parseJsonObject()
+        {
+            std::map<std::string, Json> values = {};
+            auto amountOftokens_ { tokens_.size() };
+            while(index_ < amountOftokens_)
             {
-                auto token = tokens_[index];
-                switch (token.type_)
+                auto token = tokens_[index_];
+                if(isSyntaxLiteral(token.type_))
                 {
-                    case JsonLiteral::INTEGER: return { Json{ .integer_ = std::stoi(token.content_), 
-                                                    .type_ = JsonLiteral::INTEGER }, index + 1, {} };
-                    case JsonLiteral::STRING: return { Json{ .string_ = token.content_, 
-                                                    .type_ = JsonLiteral::STRING }, index + 1, {} };
-                    case JsonLiteral::BOOLEAN: return { Json{.boolean_ = token.content_ == "true",
-                                                        .type_ = JsonLiteral::BOOLEAN }, index + 1, {} };
-                    case JsonLiteral::OPEN_LIST_BRACE: {
-                        auto [array, newIndex, error] = parseJsonArray(index + 1);
-                        return {Json{.array_ = array, .type_ = JsonLiteral::ARRAY}, newIndex, error};
+                    if(token.type_ == JsonLiteral::CLOSED_CURLY_BRACE)
+                        return { values, ++index_, {} };
+
+                    if(token.type_ == JsonLiteral::COMMA)
+                    {
+                        index_++;
+                        token = tokens_[index_];
+                    }else if(notEmpty(values))
+                    {
+                        return { {}, index_, "Expected comma after element in object." };
+                    }else
+                    {
+                        return { {}, index_, "Expected key-value pair or closing brace in object." };
                     }
-                    case JsonLiteral::OPEN_CURLY_BRACE: {
-                        auto [object, newIndex, error] = parseJsonObject(index + 1);
-                        return { Json{.object_ = std::optional(object), .type_ = JsonLiteral::OBJECT}, newIndex, error };
-                    }
-                    case JsonLiteral::NIL: return { Json{ .string_ = token.content_, 
-                                                    .type_ = JsonLiteral::NIL }, index +1, {} };
-                    default: return { {}, index, "generic parsing error. the given type is not supported." };
                 }
-                return {{}, index, "Failed to parse."};
+
+                auto [key, newIndex, error] = parse();
+                if(notEmpty(error))
+                    return { {}, index_, error };
+                
+                if(key.type_ != JsonLiteral::STRING)
+                    return { {}, index_, "Expected string key in object." };
+                
+                index_ = newIndex;
+                token = tokens_[index_];
+
+                if(token.type_ != JsonLiteral::COLON)
+                    return { {}, index_, "Expected colon after key in object." };
+                
+                index_++;
+                token = tokens_[index_];
+                auto [value, newIndex1, error1] = parse();
+                if(notEmpty(error1))
+                    return { {}, index_, error1 };
+                values[key.string_.value()] = std::move(value);
+                index_ = newIndex1;
             }
+            return { {}, ++index_, "" };
+        }
+
+        std::tuple<Json, std::size_t, std::string> parse()
+        {
+            auto token = tokens_[index_];
+            switch (token.type_)
+            {
+                case JsonLiteral::INTEGER: return { Json{ .integer_ = std::stoi(token.content_), 
+                                                .type_ = JsonLiteral::INTEGER }, ++index_, {} };
+                case JsonLiteral::STRING: return { Json{ .string_ = token.content_, 
+                                                .type_ = JsonLiteral::STRING }, ++index_, {} };
+                case JsonLiteral::BOOLEAN: return { Json{.boolean_ = token.content_ == "true",
+                                                    .type_ = JsonLiteral::BOOLEAN }, ++index_, {} };
+                case JsonLiteral::OPEN_LIST_BRACE: {
+                    index_++;
+                    auto [array, newIndex, error] = parseJsonArray();
+                    return {Json{.array_ = array, .type_ = JsonLiteral::ARRAY}, newIndex, error};
+                }
+                case JsonLiteral::OPEN_CURLY_BRACE: {
+                    index_++;
+                    auto [object, newIndex, error] = parseJsonObject();
+                    return { Json{.object_ = std::optional(object), .type_ = JsonLiteral::OBJECT}, newIndex, error };
+                }
+                case JsonLiteral::NIL: return { Json{ .string_ = token.content_, 
+                                                .type_ = JsonLiteral::NIL }, ++index_, {} };
+                default: return { {}, index_, "generic parsing error. the given type is not supported." };
+            }
+            return {{}, index_, "Failed to parse."};
+        }
     private:
         std::vector<Token> tokens_;
+        std::size_t index_{0};
     };
 }
 
