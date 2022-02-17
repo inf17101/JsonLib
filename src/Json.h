74251b7d72fa5c77f32e85330e410f4864596b75
inf17101 <oliver.klapper@web.de>
2022-02-17 11:00:26 +0000

    refractoring of json removes unneccessary local variables

--- src/Json.h
+++ src/Json.h
@@ -19,40 +19,42 @@ namespace json
         std::optional<std::map<std::string, Json>> object_;
         JsonLiteral type_;
 
+        void printSpaces(const std::size_t level) const noexcept
+        {
+            for(std::size_t i{0}; i < (level * 4); ++i)
+                std::cout << " "; 
+        }
+
         void printJsonObject(std::size_t spaceLevel) const noexcept
         {
             std::cout << "{" << std::endl;
             ++spaceLevel;
-            auto copySpaceLevel{spaceLevel};
-            ++copySpaceLevel;
-            const auto printObject = [this, copySpaceLevel](const auto& item) { 
-                printSpaces(copySpaceLevel);
-                std::cout << '\"' << item.first << '\"' << ": "; item.second.print(copySpaceLevel); std::cout << ',' << std::endl;
+            const auto printObject = [this, spaceLevel](const auto& item) { 
+                printSpaces(spaceLevel);
+                std::cout << '\"' << item.first << "\": "; item.second.print(spaceLevel); std::cout << ',' << std::endl;
             };
-            auto endIt = std::prev(end(*object_));
-            std::for_each(begin(*object_), endIt, printObject);
-            printSpaces(copySpaceLevel);
-            std::cout << '\"' << endIt->first << "\": ";
-            endIt->second.print(copySpaceLevel);
+            auto penultimateIt = std::prev(end(*object_));
+            std::for_each(begin(*object_), penultimateIt, printObject);
+            printSpaces(spaceLevel);
+            std::cout << '\"' << penultimateIt->first << "\": ";
+            penultimateIt->second.print(spaceLevel);
             std::cout << std::endl;
             printSpaces(--spaceLevel);
             std::cout << "}";
         }
 
-        void printSpaces(const std::size_t level) const noexcept
-        {
-            for(std::size_t i{0}; i < (level * 2); ++i)
-                std::cout << " "; 
-        }
-
         void printJsonArray(std::size_t spaceLevel) const noexcept
         {
             std::cout << "[" << std::endl;
-            const auto printArray = [this, spaceLevel](const auto& item) { printSpaces(spaceLevel); item.print(spaceLevel); std::cout << ',' << std::endl; };
-            std::for_each(begin(*array_), std::prev(end(*array_)), printArray);
-            decltype(auto) endIt = (*array_).back();
+            const auto printArray = [this, spaceLevel](const auto& item) { 
+                printSpaces(spaceLevel); 
+                item.print(spaceLevel); 
+                std::cout << ',' << std::endl; 
+            };
+            decltype(auto) penultimateIt = std::prev(end(*array_));
+            std::for_each(begin(*array_), penultimateIt, printArray);
             printSpaces(spaceLevel);
-            endIt.print(spaceLevel);
+            penultimateIt->print(spaceLevel);
             std::cout << std::endl;
             printSpaces(spaceLevel);
             std::cout << "]";
